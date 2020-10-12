89ffdd8a006f861dfcbc7213a44fdb4e0b70ec3f
Devin Wilson <craigvin84@yahoo.com>
2020-10-12 20:31:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.each_with_index |dwarf|
+  puts dwarf# Your code here
 end
 
 def summon_captain_planet# code an argument here
